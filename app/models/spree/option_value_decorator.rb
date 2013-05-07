36d675ee93844d21f359df56772b67c158361b0f
The Crab <nelson@safarista.com>
2013-05-07 08:32:20 +0200

    Remove :path and :url

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -6,9 +6,9 @@ Spree::OptionValue.class_eval do
 
   has_attached_file :image,
     :styles        => { :small => '40x30#', :large => '140x110#' },
-    :default_style => :small,
-    :url           => "/spree/option_values/:id/:style/:basename.:extension",
-    :path          => ":rails_root/public/spree/option_values/:id/:style/:basename.:extension"
+    :default_style => :small
+    #,:url           => "/spree/option_values/:id/:style/:basename.:extension",
+    #:path          => ":rails_root/public/spree/option_values/:id/:style/:basename.:extension"
 
   include Spree::Core::S3Support
   supports_s3 :image
