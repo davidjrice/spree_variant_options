25c38e0c8ef6d238f38f278b71dd500842abee00
The Crab <nelson@safarista.com>
2013-05-10 09:42:55 +0200

    Fix the dependency on Spree::Core::S3Support

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -6,12 +6,12 @@ Spree::OptionValue.class_eval do
 
   has_attached_file :image,
     :styles        => { :small => '40x30#', :large => '140x110#' },
-    :default_style => :small
-    #,:url           => "/spree/option_values/:id/:style/:basename.:extension",
-    #:path          => ":rails_root/public/spree/option_values/:id/:style/:basename.:extension"
+    :default_style => :small,
+    :url           => "/spree/option_values/:style/:basename.:extension",
+    :path          => ":rails_root/public/spree/option_values/:id/:style/:basename.:extension"
 
-  include Spree::Core::S3Support
-  supports_s3 :image
+  # include Spree::Core::S3Support
+  # supports_s3 :image
 
   def has_image?
     image_file_name && !image_file_name.empty?
