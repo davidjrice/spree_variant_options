f927c9f2f6cabbef1fdae035b743cd1f0c0d7b7e
The Crab <nelson@safarista.com>
2013-05-14 02:18:41 +0200

    Finally fies the Money issue

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -10,7 +10,7 @@ Spree::Variant.class_eval do
     { 
       :id    => self.id, 
       :count => self.count_on_hand, 
-      :price => number_to_currency(actual_price, unit: "&pound;")
+      :price => Spree::Money.new(actual_price).to_s
     }
   end
     
